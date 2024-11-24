d4ee1a5bca2c4c7935fecc1e84bb9f4bcbce1be1
AyeshKuruppu <ayesh2023ugc@gmail.com>
2024-11-24 12:11:13 +0530

    -001: Update home page

--- app/page.js
+++ app/page.js
@@ -15,11 +15,7 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Git version controlling basics
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
+            
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
